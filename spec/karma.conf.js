5cc497b0b80881a8d429f90b5cbac5be501eeeb0
Suraj Air <suraj@ajency.in>
2014-06-22 14:29:48 +0530

    code coverage setup complete

--- spec/karma.conf.js
+++ spec/karma.conf.js
@@ -1,67 +1,72 @@
 // Karma configuration
 // Generated on Sun Jun 15 2014 10:34:36 GMT+0530 (IST)
 
-module.exports = function (config) {
+module.exports = function ( config ) {
 
-    config.set({
+    config.set( {
 
         // base path that will be used to resolve all patterns (eg. files, exclude)
-        basePath: '../',
+        basePath : '../',
 
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['jasmine', 'requirejs'],
+        frameworks : ['jasmine', 'requirejs'],
 
         // list of files / patterns to load in the browser
-        files: [
+        files : [
             'src/require.config.js',
             'spec/javascripts/helpers/spechelper.js',
             'spec/test-main.js',
             { pattern : 'bower_components/**/*.js', included : false },
-            { pattern: 'src/**/*.js', included: false },
-            { pattern: 'spec/javascripts/specs/**/*.js', included: false }
+            { pattern : 'src/**/*.js', included : false },
+            { pattern : 'spec/javascripts/specs/**/*.js', included : false }
         ],
 
         // list of files to exclude
-        exclude: [
+        exclude : [
 
         ],
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {
-
+        preprocessors : {
+            'src/**/*.js': ['coverage']
         },
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters : ['progress', 'coverage'],
 
         // web server port
-        port: 9876,
+        port : 9876,
 
         // enable / disable colors in the output (reporters and logs)
-        colors: true,
+        colors : true,
 
         // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
+        logLevel : config.LOG_INFO,
 
         // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: true,
+        autoWatch : true,
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['PhantomJS', 'Chrome'],
+        browsers : ['PhantomJS', 'Chrome'],
 
         // disable weird "no timestamp" warning
-        client: {
-            requireJsShowNoTimestampsError: false
+        client : {
+            requireJsShowNoTimestampsError : false
+        },
+
+        coverageReporter: {
+            type : 'html',
+            dir : 'coverage/'
         },
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
-        singleRun: false
-    });
+        singleRun : false
+    } );
 };
